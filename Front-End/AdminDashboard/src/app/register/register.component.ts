c064cb1850128b4e4d30fd8430fd9c78c7362396
AhmedYahiaElasmer <ahmedyahiarabeaa2001@gmail.com>
2024-02-14 17:18:40 +0200

    ahmed yahia

--- Front-End/AdminDashboard/src/app/register/register.component.ts
+++ Front-End/AdminDashboard/src/app/register/register.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  years: number[] = [];
+  days: number[] = [];
 
+  constructor() {}
+
+  ngOnInit(): void {
+    const currentYear = new Date().getFullYear();
+    console.log(this.days);
+    for (let i = 1990; i <= currentYear; i++) {
+      this.years.push(i);
+    }
+
+    for (let i = 1; i <= 31; i++) {
+      console.log(i);
+      this.days.push(i);
+    }
+  }
 }
