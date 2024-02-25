fabe6ec37ec32a89c2e474d941016e16ff7c9c64
AhmedYahiaElasmer <ahmedyahiarabeaa2001@gmail.com>
2024-02-26 00:27:09 +0200

    logout

--- Front-End/AdminDashboard/src/app/navbar/navbar.component.ts
+++ Front-End/AdminDashboard/src/app/navbar/navbar.component.ts
@@ -1,12 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { UsersService } from '../services/users.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
+  providers: [UsersService],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  constructor(
+    private UsersService: UsersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+  logout() {
+    console.log('logout');
+    this.UsersService.setToken('');
+    this.router.navigate(['/'], {
+      relativeTo: this.route,
+    });
+  }
 }
